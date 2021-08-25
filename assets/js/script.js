b884b52fa5332149069ef2485831ed6bb7a76abf
Charlotte Clark <charlocc@usc.edu>
2021-08-24 19:59:10 -0700

    Added weatherAPI functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,36 @@ function handleSearchFormSubmit(event) {
     console.error('Please input city name');
     return;
   }
+    
+    var stored = localStorage.setItem('searchedCity', searchInputVal);
 
-  var stored = localStorage.setItem('searchedCities', searchInputVal);
-  console.log(stored);
-
-  document.location.href = 'searchactivities.html';
+    
+    searchWeatherApi(searchInputVal);
+    // document.location.href = 'searchactivities.html'; 
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+
+function searchWeatherApi(city){
+    var weatherURL = `https://weatherapi-com.p.rapidapi.com/current.json?q=${city}`
+    fetch(weatherURL, {
+        "method": "GET",
+	    "headers": {
+		"x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+		"x-rapidapi-key": "824cc48d69msh1f0d21e953db9b5p1ab70cjsne425b859ecce"
+        }
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .catch(err => {
+        console.error(err);
+    })
+    .then(function(data){
+        console.log(data)
+    })
+    var stateName = document.querySelector(".state-name");
+    stateName.textContent=location.region;
+    console.log(stateName);
+}
