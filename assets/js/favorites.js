20e805f9f3bf971d9bb4b5f5daf3efeec5070726
Jordan Scott <jscott0751@gmail.com>
2021-08-30 01:29:05 -0400

    added function to render park data to favorites page

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -4,10 +4,11 @@ var favoriteActv;
 // Get and render park data
 var favoriteParks = JSON.parse(localStorage.getItem('park'));
 
-favoriteParks.forEach(function (el) {
+favoriteParks.forEach(function (park) {
     var newListItem = document.createElement('li')
     newListItem.setAttribute('class', 'list-item')
-    newListItem.textContent = el
+    newListItem.setAttribute('style', 'font-weight: bolder;')
+    newListItem.textContent = park
     favParkEl.appendChild(newListItem)
 
 })
