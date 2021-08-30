d37e8cf8e7b326b70a3666dabd1fad2d5d2b45ec
Jordan Scott <jscott0751@gmail.com>
2021-08-29 21:11:50 -0400

    edited local storage

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -1,1 +1,11 @@
-var 
+// how to store data 
+
+//1. parks 
+//. rest
+//actovi
+ var favoriteActv;
+
+
+ var favoriteParks = JSON.parse(localStorage.getItem('park'));
+
+ console.log(favoriteParks)
