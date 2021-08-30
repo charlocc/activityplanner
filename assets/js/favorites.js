e7a8b80e861b77a5e5276d1dfae6da7339a9b340
Jordan Scott <jscott0751@gmail.com>
2021-08-30 01:23:20 -0400

    deleted li in html for fav page and created dynamically

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -1,11 +1,14 @@
-// how to store data 
+var favParkEl = document.getElementById('fav-park-list')
+var favoriteActv;
 
-//1. parks 
-//. rest
-//actovi
- var favoriteActv;
+// Get and render park data
+var favoriteParks = JSON.parse(localStorage.getItem('park'));
 
+favoriteParks.forEach(function (el) {
+    var newListItem = document.createElement('li')
+    newListItem.setAttribute('class', 'list-item')
+    newListItem.textContent = el
+    favParkEl.appendChild(newListItem)
 
- var favoriteParks = JSON.parse(localStorage.getItem('park'));
+})
 
- console.log(favoriteParks)
